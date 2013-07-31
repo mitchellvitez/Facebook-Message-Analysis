19550fb15508802481a6f8dc3d0e5dac1ca4a6d1
Mitchell Vitez <mitchellvitez@gmail.com>
2013-07-31 17:17:35 -0400

    Create out.py
    Takes the out.html file created by messages.py and adds up total numbers of each word used. Orders these and writes them to a file called "count.html"

---
+++ out.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+file = open("out.html")
+count = open("count.html", 'w')
+
+s = ""
+for line in file:
+  s += line.replace('\n','').replace('.','').replace(',','')
+
+arr = s.split(' ')
+
+print "Total words: " + str(len(arr))
+
+myDict = defaultdict(int)
+
+for word in arr:
+	myDict[word] += 1
+
+stringy = ""
+for k,v in sorted(myDict.iteritems(), key=lambda value: value[1]):
+	if v >4:
+		stringy += str(v) + " "*(5-len(str(v))) + str(k) + '\n'
+
+count.write(stringy)
+	
